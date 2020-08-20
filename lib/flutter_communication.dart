7d87a1f3b666450a5263306fcc65eb3fa4de6827
skevinhpatel <skevinhpatel@gmail.com>
2020-08-20 18:53:18 +0530

    improvment

--- lib/flutter_communication.dart
+++ lib/flutter_communication.dart
@@ -1,9 +1,11 @@
 library flutter_communication;
 
+import 'dart:collection';
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 import 'MyProgressDialog.dart';
 
@@ -19,8 +21,9 @@ class Communication {
     _dialog = MyProgressDialog(context);
   }
 
+  @deprecated
   Post(String url, Map<String, String> param, Object object,
-      {bool showProgress = true, token = ""}) async {
+      {bool showProgress = true, String token = ""}) async {
     _dialog.show(showProgress);
 
     var request = new http.Request("POST", Uri.parse(url));
@@ -36,7 +39,31 @@ class Communication {
               _setSuccess(value, object);
             }))
         .catchError((error) {
-      _setFailed(error.toString(), object);
+      _setFailed(request, error.toString(), object);
+    });
+  }
+
+  post(String url, Object object,
+      {Map<String, String> param,
+      bool showProgress = true,
+      String token = ""}) async {
+    _dialog.show(showProgress);
+    var request = new http.Request("POST", Uri.parse(url));
+    if (param != null) {
+      request.bodyFields = param;
+    }
+    request.headers.addAll({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
+    request
+        .send()
+        .then((response) => response.stream.bytesToString().then((value) {
+              _setSuccess(value, object);
+            }))
+        .catchError((error) {
+      _setFailed(request, error.toString(), object);
     });
   }
 
@@ -64,7 +91,7 @@ class Communication {
     }
   }
 
-  _setFailed(String value, Object object) {
+  _setFailed(Request request, String value, Object object) {
     _dialog.dismiss();
     listener.onCallBackFail(value, object);
 
@@ -80,14 +107,51 @@ class Communication {
                 Navigator.of(context).pop();
               },
             ),
+            FlatButton(
+              child: Text('Retry'),
+              onPressed: () {
+                _retry(request, object);
+                Navigator.of(context).pop();
+              },
+            ),
           ],
         );
       },
     );
   }
 
+  _setFailedMultiPart(MultipartRequest request, String value, Object object) {
+    _dialog.dismiss();
+    listener.onCallBackFail(value, object);
+
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Text(value),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Retry'),
+              onPressed: () {
+                _retryMultiPart(request, object);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @deprecated
   Get(String url, Map<String, String> param, Object object,
-      {showProgress = true, token = ""}) async {
+      {bool showProgress = true, String token = ""}) async {
     _dialog.show(showProgress);
     var request = new http.Request("GET", Uri.parse(url));
     request.bodyFields = param;
@@ -96,20 +160,54 @@ class Communication {
       'Accept': 'application/json',
       'Authorization': 'Bearer $token',
     });
+
     request
         .send()
         .then((response) => response.stream.bytesToString().then((value) {
               _setSuccess(value, object);
             }))
         .catchError((error) {
-      _setFailed(error.toString(), object);
+      _setFailed(request, error.toString(), object);
     });
   }
 
-  Part(String url, Map<String, String> param, File file, Object object) async {
+  @deprecated
+  get(String url, Object object,
+      {Map<String, String> param,
+      bool showProgress = true,
+      String token = ""}) async {
+    _dialog.show(showProgress);
+    var request = new http.Request("GET", Uri.parse(url));
+    if (param != null) {
+      request.bodyFields = param;
+    }
+    request.headers.addAll({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
+
+    request
+        .send()
+        .then((response) => response.stream.bytesToString().then((value) {
+              _setSuccess(value, object);
+            }))
+        .catchError((error) {
+      _setFailed(request, error.toString(), object);
+    });
+  }
+
+  @deprecated
+  Part(String url, Map<String, String> param, File file, Object object,
+      {bool showProgress = true, String token = ""}) async {
+    _dialog.show(showProgress);
     var request = new http.MultipartRequest("POST", Uri.parse(url));
     request.fields.addAll(param);
-    request.headers.addAll({'api-key': 'WLJGDnxq9u5bvYjzurmo1N2q8ljDki84'});
+    request.headers.addAll({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
     request.files.add(
         new http.MultipartFile.fromBytes('file', await file.readAsBytes()));
     request
@@ -118,13 +216,47 @@ class Communication {
               _setSuccess(value, object);
             }))
         .catchError((error) {
-      _setFailed(error.toString(), object);
+      _setFailedMultiPart(request, error.toString(), object);
     });
   }
 
-  PartList(String url, Map<String, String> param, String key,
-      List<File> fileList, Object object) async {
+  part(String url, File file, Object object,
+      {Map<String, String> param,
+      bool showProgress = true,
+      String token = ""}) async {
+    _dialog.show(showProgress);
     var request = new http.MultipartRequest("POST", Uri.parse(url));
+    if (param != null) {
+      request.fields.addAll(param);
+    }
+    request.headers.addAll({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
+    request.files.add(
+        new http.MultipartFile.fromBytes('file', await file.readAsBytes()));
+    request
+        .send()
+        .then((response) => response.stream.bytesToString().then((value) {
+              _setSuccess(value, object);
+            }))
+        .catchError((error) {
+      _setFailedMultiPart(request, error.toString(), object);
+    });
+  }
+
+  @deprecated
+  PartList(String url, Map<String, String> param, String key,
+      List<File> fileList, Object object,
+      {bool showProgress = true, String token = ""}) async {
+    _dialog.show(showProgress);
+    var request = new http.MultipartRequest("POST", Uri.parse(url));
+    request.headers.addAll({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
     request.fields.addAll(param);
     var i = 0;
     for (File file in fileList) {
@@ -137,9 +269,72 @@ class Communication {
               _setSuccess(value, object);
             }))
         .catchError((error) {
-      _setFailed(error.toString(), object);
+      _setFailedMultiPart(request, error.toString(), object);
     });
   }
+
+  partList(String url, String key, List<File> fileList, Object object,
+      {Map<String, String> param,
+      bool showProgress = true,
+      String token = ""}) async {
+    _dialog.show(showProgress);
+    var request = new http.MultipartRequest("POST", Uri.parse(url));
+    request.headers.addAll({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
+    if (param != null) {
+      request.fields.addAll(param);
+    }
+    var i = 0;
+    for (File file in fileList) {
+      request.files.add(new http.MultipartFile.fromBytes(
+          key + '[${i}]', await file.readAsBytes()));
+    }
+    request
+        .send()
+        .then((response) => response.stream.bytesToString().then((value) {
+              _setSuccess(value, object);
+            }))
+        .catchError((error) {
+      _setFailedMultiPart(request, error.toString(), object);
+    });
+  }
+
+  _retry(request, object) {
+    request
+        .send()
+        .then((response) => response.stream.bytesToString().then((value) {
+              _setSuccess(value, object);
+            }))
+        .catchError((error) {
+      _setFailed(request, error.toString(), object);
+    });
+  }
+
+  _retryMultiPart(request, object) {
+    request
+        .send()
+        .then((response) => response.stream.bytesToString().then((value) {
+              _setSuccess(value, object);
+            }))
+        .catchError((error) {
+      _setFailed(request, error.toString(), object);
+    });
+  }
+
+/*Download(String url, File file) {
+    HttpClient client = new HttpClient();
+    client
+        .getUrl(Uri.parse(
+            "https://fluttermaster.com/wp-content/uploads/2018/08/fluttermaster.com-logo-web-header.png"))
+        .then((HttpClientRequest request) {
+      return request.close();
+    }).then((HttpClientResponse response) {
+      response.pipe(file.openWrite());
+    });
+  }*/
 }
 
 abstract class OnCommunicationListener {
